4e5db9a20fd88fa51aaf19e3d239a1904ffce0f0
Krishna <lkrushana@gmail.com>
2024-05-26 20:28:28 +0530

    import and export in js

---
+++ script.js
@@ -0,0 +1,5 @@
+var a = 12;
+
+console.log(a);
+
+module.exports= a; //we can export the existing data to another file 
---
+++ script2.js
@@ -0,0 +1,3 @@
+var data = require("./script.js");  //by using this we import all data from script file
+
+console.log(data);
