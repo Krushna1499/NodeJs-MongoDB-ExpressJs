1e2a07aaad828ce967dafe063de5fba75047c2e7
Krishna <lkrushana@gmail.com>
2024-05-27 10:18:34 +0530

    Basics

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var a = 12;
+// var a = 12;
 
-console.log(a);
+// console.log(a);
 
-module.exports= a; //we can export the existing data to another file 
+// module.exports= a; //we can export the existing data to another file
+var figlet = require("figlet");
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
--- script2.js
+++ script2.js
@@ -1,3 +1,15 @@
-var data = require("./script.js");  //by using this we import all data from script file
+// var data = require("./script.js");  //by using this we import all data from script file
 
-console.log(data);
+// console.log(data);
+const express = require('express');
+const app = express();
+
+app.get('/',function(req,res)
+{
+    res.send("hello haye hoii");
+});
+app.get('/profile',function(req,res)
+{
+    res.send("hello profile");
+});
+app.listen(3000);
